34f15a9a17997f36eb2bff0825b9e33cce223309
Frank Villasenor <frank.villasenor@gmail.com>
2017-11-02 06:35:44 -0500

    Inital project; basic hello world.

---
+++ RiderSampleNancy/SampleModule.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using Nancy;
+
+namespace RiderSampleNancy
+{
+    public class SampleModule : Nancy.NancyModule
+    {
+        private struct counterObj
+        {
+            public int callCount = 0;
+        }
+        
+        private counterObj pp = new counterObj();
+        
+        public SampleModule()
+        {
+            
+            
+            Get["/"] = arg => "Hello World!"; //This works; passing data is not so successful.
+            //Get("/", arg => string.Format("Hello world! {0}", pp.callCount++));
+            //Get("/", arg => string.Format("Hello world! {0}", arg++));
+        }
+
+    }
+}
